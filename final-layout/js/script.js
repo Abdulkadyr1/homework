a381ce206d9a824585e4fcdf2e66930f44bf7880
Abdulkadyr <damadevkadir10@gmail.com>
2025-05-05 18:18:36 +0300

    about-section

--- final-layout/js/script.js
+++ final-layout/js/script.js
@@ -1,6 +1,9 @@
 const burgerIcon = document.querySelector('.burger-icon')
 const body = document.querySelector('.body')
 const navList = document.querySelector('.header__list')
+const modalGift = document.querySelector('.about__modal-link')
+const modalWindowOpen = document.querySelector('.modal__window')
+const modalWindowClose = document.querySelector('.modal__window-cancel')
 
 burgerIcon.addEventListener('click', ()=>{
     body.classList.toggle('body--opened-menu')
@@ -11,4 +14,21 @@ navList.addEventListener('click', (event)=>{
     if (target.classList.contains('header__link')){
         body.classList.remove('body--opened-menu')
     }
+})
+
+modalGift.addEventListener('click', (event) =>{
+    event.preventDefault()
+    modalWindowOpen.classList.add('modal-open')
+})
+
+modalWindowOpen.addEventListener('click', (event) =>{
+    if (event.target && event.target.classList.contains('modal__window')){
+        modalWindowOpen.classList.remove('modal-open')
+        console.log("HElo")
+    }
+})
+
+modalWindowClose.addEventListener('click', (event) =>{
+    event.preventDefault()
+    modalWindowOpen.classList.remove('modal-open')
 })
